18ed1c9d0ecd5e5e4de8218cd9ffeaafc156eff0
Stefania Overman <stefaniaoverman@gmail.com>
2018-08-30 12:26:58 -0300

    Agrego carpeta de proyecto tienda

---
+++ proyectos/router-app/src/components/categoria/categoria.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+  <h1> Esta es la pagina de Categorias</h1>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'categoria',
+  data () {
+    return {
+    }
+  },
+}
+
+  </script>
