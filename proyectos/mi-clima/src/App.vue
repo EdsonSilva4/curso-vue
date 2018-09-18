1de871241c5350739038266d4311f800f947aee0
Rodrigo Juliani <RJuliani@altimetrik.com>
2018-09-18 18:31:01 -0300

    agrego axios y ejemplo de promises

--- proyectos/mi-clima/src/App.vue
+++ proyectos/mi-clima/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <posicion/>
+    <recurso-remoto />
   </div>
 </template>
 
 <script>
 import Posicion from '@/playground/posicion.vue'
+import RecursoRemoto from '@/playground/recursoremoto.vue'
 
 export default {
   name: 'app',
   components: {
-    Posicion
+    Posicion,
+    RecursoRemoto
   }
 }
 </script>
---
+++ proyectos/mi-clima/src/playground/recursoremoto.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>recurso remoto</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+  export default {
+    created () {
+      var promesa = new Promise(function(resolve, reject) {
+        reject({ error: 'Hubo un problema al procesar la solicitud'})
+      })
+      promesa.then(function(success) {
+        console.log(success)
+        alert(success.mensaje)
+      }).catch(function(error) {
+        console.log(error)        
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
